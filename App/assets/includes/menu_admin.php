26a8598fd03423bbc52f79e7e2667a810f0feae8
rsouza28 <rodrigo.amaral@uniriotec.br>
2017-12-02 01:56:46 -0200

    Update menu_admin.php
    alteração pontual

--- App/assets/includes/menu_admin.php
+++ App/assets/includes/menu_admin.php
@@ -14,7 +14,7 @@
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="color:#eeeeee;"><span class='glyphicon glyphicon-user' aria-hidden='true'> </span> <?php echo $_SESSION["nome"];?><span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="homeadmin.php"> <span class='glyphicon glyphicon-home' aria-hidden='true'> </span> Home</a></li>  
+          <li><a href="index.php"> <span class='glyphicon glyphicon-home' aria-hidden='true'> </span> Home</a></li>  
           <li role="separator" class="divider"></li>
           
           <li><a href="administradores.php"> <span class='glyphicon glyphicon-user' aria-hidden='true'> </span> Administradores</a></li>
